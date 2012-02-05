ead751bf3e8213c9af40b1e77b26c864a8889735
hofmockel <dreagonfly@gmx.de>
2012-02-05 11:08:46 +0100

    Add a setup.py and MANIFEST file for proper packeing

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from setuptools import Extension
+
+mod = Extension(
+        'boost_queue',
+        sources=['boost_queue.cpp'],
+        libraries=['boost_thread', 'boost_date_time'],
+        extra_compile_args=["-O2"],
+        depends=['concurrent_queue.hpp']
+        )
+
+setup(
+    name='boost_queue',
+    version='0.1',
+    description="Queue using boost's locking API",
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Programming Language :: C++',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        ],
+    keywords='queue boost',
+    author='Stephan Hofmockel',
+    url='https://github.com/stephan-hof/boost_queue',
+    license='boost',
+    packages=['tests'],
+    ext_modules=[mod]
+)
